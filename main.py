670b0d27b0ffce6e01bdb4bf344f63fd1a538db1
chaereeP <49869873+chaereeP@users.noreply.github.com>
2019-11-04 17:49:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from utils.schedule import LinearSchedule
 import simulator
 
 BATCH_SIZE = 32
-GAMMA = 0.99
+GAMMA = 0.99 # 시간 지날 수록 감마 곱해서 reward
 REPLAY_BUFFER_SIZE = 1000000
 LEARNING_STARTS = 10000
-LEARNING_FREQ = 4
-FRAME_HISTORY_LEN = 4
+LEARNING_FREQ = 4 
+FRAME_HISTORY_LEN = 4 #image 4겹쳐서
 TARGER_UPDATE_FREQ = 10000
-LEARNING_RATE = 0.00025
-ALPHA = 0.95
-EPS = 0.01
+LEARNING_RATE = 0.00025 
+ALPHA = 0.95 # 클수록 빠르게 되지만 크게 변동, 점점 작게 #처음은 크게, 점점 작게 alpha
+EPS = 0.01 #입실론
 
 def main(env):
 
